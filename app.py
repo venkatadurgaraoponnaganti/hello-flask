c988dd7302e21d58bc528cc41ec5433250b0fb85
Ubuntu <ubuntu@ip-172-31-12-169.ap-south-1.compute.internal>
2025-11-12 05:03:20 +0000

    version-1.0

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("name", "").strip() or None
+    return render_template("hello.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
